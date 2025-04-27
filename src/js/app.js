001d979b63cb465bcd99cce467a4ed3778490d6d
Anton Pakhomov <antonn.pakhomovv@gmail.com>
2025-04-27 14:31:23 +0400

    add render

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,32 @@
 (async () => {
   try {
     if (navigator.serviceWorker) {
-      await navigator.serviceWorker.register('/service-worker.js');
-      console.log('sw registered');
+      await navigator.serviceWorker.register("/service-worker.js");
+      console.log("sw registered");
     }
   } catch (e) {
     console.log(e);
   }
 })();
 
+// const url = "https://service-worker-loading-backend.onrender.com";
+
 function noNetwork() {
-  const container = document.querySelector('.news-container');
+  const container = document.querySelector(".news-container");
 
   container.insertAdjacentHTML(
-    'afterbegin',
+    "afterbegin",
     `
       <div class="modal">
         <p class="modal-text">
           Не удалось загрузить данные Проверьте подключение и обновите страницу
         </p>
       </div>
-      `,
+      `
   );
 }
 
-fetch('http://localhost:7070/api/test')
+fetch("http://localhost:7070/api/test")
   .then((r) => r.json())
   .then((r) => console.log(r))
   .catch(() => {
